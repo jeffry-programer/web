f5151705a67545786eefa8eac29c08d16e4f5c42
Jeffry <jeffryavellaneda@gmail.com>
2024-02-16 11:14:52 -0400

    'ajustes'

--- database/migrations/2023_11_29_223945_create_stores_table.php
+++ database/migrations/2023_11_29_223945_create_stores_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('stores', function (Blueprint $table) {
+        Schema::create('stores', function (Blueprint $table){
             $table->id();
             $table->foreignId('type_stores_id');
             $table->foreignId('users_id');
