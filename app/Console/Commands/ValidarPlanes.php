bc54c49c9c8549f4979ca8eb1f54ce171c1e585f
Jeffry <jeffryavellaneda@gmail.com>
2024-10-17 18:39:56 +0000

    adjust api

--- app/Console/Commands/ValidarPlanes.php
+++ app/Console/Commands/ValidarPlanes.php
@@ -9,6 +9,9 @@ use App\Notifications\PlanPorVencer;
 use App\Notifications\PlanVencidoNotification;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Crypt;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Messaging\CloudMessage;
 
 class ValidarPlanes extends Command
 {
@@ -56,14 +59,47 @@ class ValidarPlanes extends Command
 
             $tienda->user->notify(new PlanVencidoNotification($tienda, 'Tulobuscas tu plan vence hoy'));
             $user_admin->notify(new PlanVencidoNotification($tienda, 'Vence hoy plan para la tienda '.$tienda->name));
+
+            $this->sendNotification($tienda, 'El plan de tu negocio vence hoy.');
         }
 
         // Para las tiendas con plan que vence en 5 días
         foreach ($tiendasConPlanPorVencer as $tienda) {
             // Envía correo para plan a punto de vencer
             $tienda->user->notify(new PlanPorVencer($tienda));
+            $this->sendNotification($tienda, 'El plan de tu negocio vence en 5 días.');
         }
 
         $this->info('Planes vencidos validados y tiendas actualizadas exitosamente.');
+    }
+
+    public function sendNotification($store, $message){
+        $token = $store->user->token;
+        if (strlen($token) > 10) {
+            $firebase = (new Factory)->withServiceAccount(base_path(env('FIREBASE_CREDENTIALS')));
+
+            // Obtener el servicio de mensajería
+            $messaging = $firebase->createMessaging();
+
+            // Crear el mensaje
+            $message = CloudMessage::fromArray([
+                'token' => $token,  // El token del dispositivo que recibirá la notificación
+                'notification' => [
+                    'title' => 'Tulobuscas',
+                    'body' => $message,
+                    'icon' => 'https://tulobuscas.app/images/tulobuscas2.png', // URL de la imagen del ícono de la notificación
+                ],
+                'data' => [ // Datos adicionales para manejar la redirección
+                    'click_action' => 'OPEN_URL',
+                    'url' => '/detail-store/' . $store->id,  // Ruta donde quieres redirigir al usuario
+                ],
+                'android' => [  // Mover el bloque de Android fuera de 'data'
+                    'priority' => 'high',
+                ],
+            ]);
+
+            // Enviar el mensaje
+            $messaging->send($message);
+        }
     }
 }
